8df8760f01e73d4f798a86471f8aab2ae40390cf
YashwanthThonukunuru <yash12355@gmail.com>
2024-04-03 21:39:18 +0200

    first commit

---
+++ models/src/src_hosts.sql
@@ -0,0 +1,12 @@
+WITH raw_hosts AS (
+    SELECT
+        *
+    FROM {{source('airbnb', 'hosts')}}
+)
+SELECT 
+    ID AS HOST_ID,
+    NAME AS HOST_NAME,
+    IS_SUPERHOST,
+    CREATED_AT,
+    UPDATED_AT
+FROM raw_hosts
